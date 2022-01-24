da16ed011f5e51ca622b9c14834391acad2ba3a4
Mosesteshome <64324115+Mosesteshome@users.noreply.github.com>
2022-01-24 16:24:48 +0300

    leetcode sort

---
+++ Task 5/Max Number of K-Sum Pairs.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def maxOperations(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        
+        nums.sort()
+        count = 0 
+        
+        i, j = 0, len(nums)-1
+        while i < j:
+            if nums[i] + nums[j] == k:
+                count += 1
+                i += 1
+                j -= 1
+            elif nums[i] + nums[j] > k:
+                j -= 1
+            else:
+                i +=1
+        return count
